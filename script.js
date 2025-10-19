ecd0b54372147c753c1b2b2c1cd85db520991842
fahu087 <fahadsaudagar333@gmail.oc>
2025-10-20 00:04:25 +0530

    six

---
+++ script.js
@@ -0,0 +1,11 @@
+function showDate() {
+  document.getElementById('date').style.display = 'block';
+  playMusicOnce();
+  document.getElementById('dateBtn').style.display = 'none'; // hide button
+}
+
+function showVenue() {
+  document.getElementById('venue').style.display = 'block';
+  playMusicOnce();
+  document.getElementById('venueBtn').style.display = 'none'; // hide button
+}
